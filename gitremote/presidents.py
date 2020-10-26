44e6776268734416dc3b0a27f2ce15ebcc3f20bf
Brandon Adrian <brandonadrian110@gmail.com>
2020-10-25 22:29:57 -0400

    added president code

--- gitremote/presidents.py
+++ gitremote/presidents.py
@@ -1,7 +1,18 @@
+import requests
+
+
+url_ddg = "https://api.duckduckgo.com"
 
 def get_presidents():
-    return "hello"
+    q = "presidents of the united states"
+    resp = requests.get(url_ddg + "/?q=" + q + "&format=json")
+
+    rsp_data = resp.json()
+
+    return rsp_data["RelatedTopics"]
 
 
 def main():
     print(get_presidents())
+
+main()
--- gitremote/tests/test_presidents.py
+++ gitremote/tests/test_presidents.py
@@ -3,5 +3,56 @@ import pytest
 from gitremote.presidents import get_presidents
 
 
+presidents_list = [
+    "Washington",
+    "Adams",
+    "Jefferson",
+    "Madison",
+    "Monroe",
+    "Jackson",
+    "Buren",
+    "Harrison",
+    "Tyler",
+    "Polk",
+    "Taylor",
+    "Fillmore",
+    "Pierce",
+    "Buchanan",
+    "Lincoln",
+    "Johnson",
+    "Grant",
+    "Hayes",
+    "Garfield",
+    "Arthur",
+    "Cleveland",
+    "Harrison",
+    "Cleveland",
+    "McKinley",
+    "Roosevelt",
+    "Taft",
+    "Wilson",
+    "Harding",
+    "Coolidge",
+    "Hoover",
+    "Roosevelt",
+    "Truman",
+    "Eisenhower",
+    "Kennedy",
+    "Johnson",
+    "Nixon",
+    "Ford",
+    "Carter",
+    "Reagan",
+    "Bush",
+    "Clinton",
+    "Obama",
+    "Trump",
+]
+
 def test_get_presidents():
-    assert get_presidents() == "hello"
+    rsp_data = get_presidents()
+
+    for result in rsp_data:
+        assert any(word in result["Text"] for word in presidents_list), "Could not find a president in " + result["Text"]
+
+
