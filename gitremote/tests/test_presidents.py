1b3c6d1094a5b7c65d7f400dd5bd11acbfbf9a63
Brandon Adrian <brandonadrian110@gmail.com>
2020-10-26 20:26:14 -0400

    Added logic and fixed pytests

--- gitremote/tests/test_presidents.py
+++ gitremote/tests/test_presidents.py
@@ -4,55 +4,65 @@ from gitremote.presidents import get_presidents
 
 
 presidents_list = [
-    "Washington",
-    "Adams",
-    "Jefferson",
-    "Madison",
-    "Monroe",
-    "Jackson",
-    "Buren",
-    "Harrison",
-    "Tyler",
-    "Polk",
-    "Taylor",
-    "Fillmore",
-    "Pierce",
-    "Buchanan",
-    "Lincoln",
-    "Johnson",
-    "Grant",
-    "Hayes",
-    "Garfield",
-    "Arthur",
-    "Cleveland",
-    "Harrison",
-    "Cleveland",
-    "McKinley",
-    "Roosevelt",
-    "Taft",
-    "Wilson",
-    "Harding",
-    "Coolidge",
-    "Hoover",
-    "Roosevelt",
-    "Truman",
-    "Eisenhower",
-    "Kennedy",
-    "Johnson",
-    "Nixon",
-    "Ford",
-    "Carter",
-    "Reagan",
-    "Bush",
-    "Clinton",
-    "Obama",
-    "Trump",
+    ("Washington", False),
+    ("Adams", False),
+    ("Jefferson", False),
+    ("Madison", False),
+    ("Monroe", False),
+    ("Jackson", False),
+    ("Buren", False),
+    ("Harrison", False),
+    ("Tyler", False),
+    ("Polk", False),
+    ("Taylor", False),
+    ("Fillmore", False),
+    ("Pierce", False),
+    ("Buchanan", False),
+    ("Lincoln", False),
+    ("Johnson", False),
+    ("Grant", False),
+    ("Hayes", False),
+    ("Garfield", False),
+    ("Arthur", False),
+    ("Cleveland", False),
+    ("Harrison", False),
+    ("Cleveland", False),
+    ("McKinley", False),
+    ("Roosevelt", False),
+    ("Taft", False),
+    ("Wilson", False),
+    ("Harding", False),
+    ("Coolidge", False),
+    ("Hoover", False),
+    ("Roosevelt", False),
+    ("Truman", False),
+    ("Eisenhower", False),
+    ("Kennedy", False),
+    ("Johnson", False),
+    ("Nixon", False),
+    ("Ford", False),
+    ("Carter", False),
+    ("Reagan", False),
+    ("Bush", False),
+    ("Clinton", False),
+    ("Obama", False),
+    ("Trump", False)
 ]
 
 def test_get_presidents():
+    original_length = len( presidents_list )
     rsp_data = get_presidents()
 
     for result in rsp_data:
-        assert any(word in result["Text"] for word in presidents_list), "Could not find a president in " + result["Text"]
+        for item in ((word[0], word[0] in result["Text"]) for word in presidents_list):
+            index = -1
+            try:
+                index = presidents_list.index((item[0], False))
+            except ValueError:
+                index = -1
 
+            if item[1] == True and index >= 0:
+                presidents_list.pop(index)
+                presidents_list.append((item[0], True))
 
+    assert all(word[1] == True for word in presidents_list) and len(presidents_list) == original_length
